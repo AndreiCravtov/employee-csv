327930dc5bd82ea070b3b44687d42627676648d8
Andrei Cravtov <the.andrei.cravtov@gmail.com>
2022-09-20 16:06:46 +0100

    push code

--- src/main/java/com/sparta/Employee.java
+++ src/main/java/com/sparta/Employee.java
@@ -1,5 +1,27 @@
 package com.sparta;
 
 public class Employee {
+    int employeeID;
+    String namePrefix;
+    String firstName;
+    String middleInitial;
+    String lastName;
+    char gender;
+    String eMail;
+    String dateOfBirth;
+    String dateOfJoining;
+    int salary;
 
+    public Employee(int employeeID) {
+        this.employeeID = employeeID;
+    }
+
+    /**
+     * This method serializes the employee object.
+     * It turns the fields in the
+     * @return
+     */
+    public String serialize() {
+        return "";
+    }
 }
