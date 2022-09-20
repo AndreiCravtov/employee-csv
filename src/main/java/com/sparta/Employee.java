8cc317162b92728a2211cedf7b7eeefa9d8ad16e
Andrei Cravtov <the.andrei.cravtov@gmail.com>
2022-09-20 16:34:05 +0100

    push code

--- src/main/java/com/sparta/Employee.java
+++ src/main/java/com/sparta/Employee.java
@@ -1,19 +1,82 @@
 package com.sparta;
 
+/**
+ *
+ */
 public class Employee {
-    int employeeID;
-    String namePrefix;
-    String firstName;
-    String middleInitial;
-    String lastName;
-    char gender;
-    String eMail;
-    String dateOfBirth;
-    String dateOfJoining;
-    int salary;
+    private int employeeID;
+    private String namePrefix;
+    private String firstName;
+    private char middleInitial;
+    private String lastName;
+    private char gender;
+    private String eMail;
+    private String dateOfBirth;
+    private String dateOfJoining;
+    private int salary;
 
-    public Employee(int employeeID) {
+    public int getEmployeeID() {
+        return employeeID;
+    }
+
+    public String getNamePrefix() {
+        return namePrefix;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public char getMiddleInitial() {
+        return middleInitial;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public char getGender() {
+        return gender;
+    }
+
+    public String geteMail() {
+        return eMail;
+    }
+
+    public String getDateOfBirth() {
+        return dateOfBirth;
+    }
+
+    public String getDateOfJoining() {
+        return dateOfJoining;
+    }
+
+    public int getSalary() {
+        return salary;
+    }
+
+    public Employee(
+            int employeeID,
+            String namePrefix,
+            String firstName,
+            char middleInitial,
+            String lastName,
+            char gender,
+            String eMail,
+            String dateOfBirth,
+            String dateOfJoining,
+            int salary
+    ) {
         this.employeeID = employeeID;
+        this.namePrefix = namePrefix;
+        this.firstName = firstName;
+        this.middleInitial = middleInitial;
+        this.lastName = lastName;
+        this.gender = gender;
+        this.eMail = eMail;
+        this.dateOfBirth = dateOfBirth;
+        this.dateOfJoining = dateOfJoining;
+        this.salary = salary;
     }
 
     /**
