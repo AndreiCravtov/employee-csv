f9a11916f3ef892edeaee51b80616f598381a72e
BuildTools <unconfigured@null.spigotmc.org>
2022-09-21 17:39:23 +0100

    csv to string class

--- src/main/java/com/sparta/CSVConverter.java
+++ src/main/java/com/sparta/CSVConverter.java
@@ -2,27 +2,22 @@ package com.sparta;
 import java.io.FileReader;
 import java.io.BufferedReader;
 import java.io.IOException;
+import java.io.FileNotFoundException;
 
 /**
  * A to convert a .csv file to a string
  */
 public class CSVConverter {
     static String convert(String fileName) {
-        try (FileReader reader = new FileReader(fileName)) {
+        try (FileReader reader = new FileReader("EmployeeRecords1.csv")) {
             BufferedReader br =new BufferedReader(reader);
             String line = "";
-            String[] tempArr;
-            String records = "";
+            String s = "";
+            final StringBuffer buffer = new StringBuffer(2048);
             while ((line = br.readLine()) != null) {
-                tempArr = line.split("");
-                for (String tempStr : tempArr) {
-                    records = records + tempArr;
-                }
-                br.close();
-
-
+                s += line;
             }
-            return records;
+            return s;
         } catch (FileNotFoundException e) {
             throw new RuntimeException(e);
         } catch (IOException e) {
