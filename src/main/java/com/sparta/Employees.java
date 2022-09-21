7a8d41f6edd985ca9d2cd768ff67ac455b11480e
Andrei Cravtov <the.andrei.cravtov@gmail.com>
2022-09-21 16:32:38 +0100

    modify

--- src/main/java/com/sparta/Employees.java
+++ src/main/java/com/sparta/Employees.java
@@ -1,22 +1,28 @@
 package com.sparta;
 
 import com.sparta.exceptions.EmployeeDifferentDataSameID;
+import com.sparta.util.TrackedHashMap;
 
 import java.util.*;
 
 public class Employees {
     private final String header = "Emp ID,Name Prefix,First Name,Middle Initial,Last Name,Gender,E Mail,Date of Birth,Date of Joining,Salary\n";
-    private final Map<Integer, Employee> employees = new HashMap<>();
+    private final TrackedHashMap<Integer, Employee> employees = new TrackedHashMap<>();
     private List<Employee> sortedEmployeesCache = new ArrayList<>();
 
-    private void updateSortedEmployeesCache() {
+    private List<Employee> getSortedEmployees() {
+        if (!employees.mapChanged) {
+            System.out.println("uses cache");
+            return sortedEmployeesCache;
+        }
         sortedEmployeesCache = new ArrayList<>(employees.values());
         Collections.sort(sortedEmployeesCache);
+        employees.mapChanged = false;
+        return sortedEmployeesCache;
     }
 
     public Employee[] getEmployees() {
-        updateSortedEmployeesCache();
-        return sortedEmployeesCache.toArray(new Employee[0]);
+        return getSortedEmployees().toArray(new Employee[0]);
     }
 
     public void addEmployee(Employee employee) throws EmployeeDifferentDataSameID {
@@ -49,10 +55,8 @@ public class Employees {
 
     public String serialize() {
         StringBuilder builder = new StringBuilder(header);
-        updateSortedEmployeesCache();
-        for (Employee employee: sortedEmployeesCache) {
+        for (Employee employee: getSortedEmployees())
             builder.append(employee.serialize()).append("\n");
-        }
         return builder.toString().strip();
     }
 }
---
+++ src/main/java/com/sparta/util/TrackedHashMap.java
@@ -0,0 +1,73 @@
+package com.sparta.util;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.function.BiFunction;
+
+public class TrackedHashMap<K, V> extends HashMap<K, V> {
+    public boolean mapChanged = false;
+
+    public TrackedHashMap() {
+        super();
+    }
+
+    @Override
+    public void clear() {
+        mapChanged = true;
+        super.clear();
+    }
+
+    @Override
+    public V merge(K key, V value, BiFunction<? super V,? super V,? extends V> remappingFunction) {
+        mapChanged = true;
+        return super.merge(key, value, remappingFunction);
+    }
+
+    @Override
+    public V put(K key, V value) {
+        mapChanged = true;
+        return super.put(key, value);
+    }
+
+    @Override
+    public void putAll(Map<? extends K,? extends V> m) {
+        mapChanged = true;
+        super.putAll(m);
+    }
+
+    @Override
+    public V putIfAbsent(K key, V value) {
+        mapChanged = true;
+        return super.putIfAbsent(key, value);
+    }
+
+    @Override
+    public V remove(Object key) {
+        mapChanged = true;
+        return super.remove(key);
+    }
+
+    @Override
+    public boolean remove(Object key, Object value) {
+        mapChanged = true;
+        return super.remove(key, value);
+    }
+
+    @Override
+    public V replace(K key, V value) {
+        mapChanged = true;
+        return super.replace(key, value);
+    }
+
+    @Override
+    public boolean replace(K key, V oldValue, V newValue) {
+        mapChanged = true;
+        return super.replace(key, oldValue, newValue);
+    }
+
+    @Override
+    public void replaceAll(BiFunction<? super K,? super V,? extends V> function) {
+        mapChanged = true;
+        super.replaceAll(function);
+    }
+}
