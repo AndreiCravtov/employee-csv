4e0f8c69eb3708d1341be2c6ee8d2b7debde2a70
Daniel Booth <unconfigured@null.spigotmc.org>
2022-09-22 16:17:37 +0100

    erroneous data

--- src/main/java/com/sparta/Main.java
+++ src/main/java/com/sparta/Main.java
@@ -4,7 +4,7 @@ public class Main {
     public static void main(String[] args) throws Exception {
         Employees employees = new Employees();
         CSVConverter.convert("src/main/resources/EmployeeRecordsLarge.csv", employees);
-        System.out.println(employees);
+        System.out.println(employees.erroneous);
         System.out.println(employees.getEmployeeCount());
     }
 }
