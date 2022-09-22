cfa2db847dfc53290f4f86a972399631e53936c3
Daniel Booth <unconfigured@null.spigotmc.org>
2022-09-22 13:28:55 +0100

    csv converter takes an employee now

--- src/main/java/com/sparta/CSVConverter.java
+++ src/main/java/com/sparta/CSVConverter.java
@@ -8,17 +8,17 @@ import java.io.FileNotFoundException;
  * A to convert a .csv file to a string
  */
 public class CSVConverter {
-    static String convert(String fileName) {
-        try (FileReader reader = new FileReader("EmployeeRecords1.csv")) {
+    static void convert(String fileName, Employees employees) {
+        try (FileReader reader = new FileReader(fileName)) {
             BufferedReader br =new BufferedReader(reader);
             String s = "";
             br.readLine();
             String line1=null;
             final StringBuffer buffer = new StringBuffer(2048);
             while ((line1 = br.readLine()) != null) {
-                s += line1 + "\n";
+                s = line1 + "\n";
+                employees.addEmployee(s);
             }
-            return s;
         } catch (FileNotFoundException e) {
             throw new RuntimeException(e);
         } catch (IOException e) {
